2025be19ad757588354dc22d797b2388c84f1c58
Bryan C <7554866+bryancandi@users.noreply.github.com>
2026-01-19 17:02:16 -0700

    Fix indentation for consistency

--- MASM/masm.asm
+++ MASM/masm.asm
@@ -2,16 +2,15 @@
 ; MASM: A barebones template for Assembly programming.
 ;--------------------------------------------------------------------
 
-INCLUDELIB kernel32.lib	    ; Import a standard Windows library.
-ExitProcess PROTO           ; Declare the ExitProcess function prototype.
+INCLUDELIB kernel32.lib         ; Import a standard Windows library.
+ExitProcess PROTO               ; Declare the ExitProcess function prototype.
 
-.DATA                       ; Start of the data section.
-                            ; Variable declarations go here.
+.DATA                           ; Start of the data section.
+                                ; Variable declarations go here.
 
-.CODE						; Start of the code section.
-main PROC					; Entry point of the program.
-                            ; Assembly instructions go here.
-CALL ExitProcess            ; Call the ExitProcess function to exit the program.
-main ENDP					; End of the main procedure.
-
-END     					; End of the assembly program.
+.CODE                           ; Start of the code section.
+main PROC                       ; Entry point of the program.
+                                ; Assembly instructions go here.
+    CALL ExitProcess            ; Call the ExitProcess function to exit the program.
+main ENDP                       ; End of the main procedure.
+END                             ; End of the assembly program.
