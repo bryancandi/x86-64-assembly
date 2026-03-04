4b3c50e693ed944d702ba9f8d638c1b8390a2464
Bryan C <7554866+bryancandi@users.noreply.github.com>
2026-03-03 18:18:12 -0700

    Factorial: refactor variable names for clarity

--- MASM/projects/fact.asm
+++ MASM/projects/fact.asm
@@ -18,7 +18,7 @@ CommandLineToArgvW PROTO
 STD_OUTPUT_HANDLE EQU -11
 
 .DATA
-    txt BYTE 100 DUP (?)        ; Empty 100 byte buffer to store ASCII digits of factorial result.
+    buf BYTE 100 DUP (?)        ; Empty 100 byte buffer to store ASCII digits of factorial result.
     handle QWORD ?              ; 64-bit storage for Windows HANDLE (returned by GetStdHandle).
     num DWORD ?                 ; 32-bit output variable for WriteConsoleA character count.
     argc DWORD ?                ; 32‑bit storage for the argument count returned by CommandLineToArgvW.
@@ -26,15 +26,15 @@ STD_OUTPUT_HANDLE EQU -11
 
 .CODE
 IntToStr PROC
-    LEA RDI, txt                ; Load address of 'txt' buffer into RDI register.
-    ADD RDI, LENGTHOF txt - 1   ; Adjust pointer to end of 'txt' buffer.
+    LEA RDI, buf                ; Load address of 'buf' buffer into RDI register.
+    ADD RDI, LENGTHOF buf - 1   ; Adjust pointer to end of 'buf' buffer.
     MOV RBX, 10                 ; Move divisor into RBX.
 
     div_loop:
     XOR RDX, RDX                ; Clear for remainder.
     DIV RBX                     ; Divide RAX by RBX (10).
     ADD RDX, '0'                ; Add ASCII '0' value to remainder in RDX to convert from integer to ASCII value.
-    MOV [RDI], DL               ; Move one byte of RDX (DL) into RDI (RDI points to 'txt' buffer).
+    MOV [RDI], DL               ; Move one byte of RDX (DL) into RDI (RDI points to 'buf' buffer).
     DEC RDI                     ; Decrement RDI; we are writing to the buffer in reverse order (starting at the end).
     TEST RAX, RAX               ; Is RAX zero?
     JNZ div_loop                ; No; restart loop.
@@ -59,7 +59,7 @@ StrToInt PROC
     RET
 StrToInt ENDP
 
-Fact PROC
+Factorial PROC
     MOV RAX, 1                  ; Initialize factorial result to 1. RAX will hold the running factorial result.
     CMP RCX, 1                  ; Check if RCX is less than or equal to 1 (fnum); return if true.
     JLE finish
@@ -72,36 +72,37 @@ Fact PROC
 
     finish:
     RET
-Fact ENDP
+Factorial ENDP
 
-main PROC                       ; Entry point of the program.
+main PROC
     SUB RSP, 40                 ; Create shadow space for 4 arguments (32 shadow + 8 alignment).
 
     CALL GetCommandLineW
     MOV RCX, RAX                ; LPWSTR cmdline.
     LEA RDX, argc               ; int argc.
+
     CALL CommandLineToArgvW
     MOV argv, RAX               ; LPWSTR* argv.
     MOV RCX, [argv]             ; Point to argv array in RCX.
     MOV RCX, [RCX+8]            ; Point to argv[1] (skip program name).
 
-    CALL StrToInt             ; Convert the string in argv[1] to an integer to use in factorial calculation.
+    CALL StrToInt               ; Convert the string in argv[1] to an integer to use in factorial calculation.
     MOV RCX, RAX                ; Move number to calculate the factorial of into RCX register.
-    CALL Fact                   ; Store factorial integer in RAX register for division in 'IntToStr'.
-    CALL IntToStr             ; Convert the integer in RAX to a string in 'txt' buffer.
+    CALL Factorial              ; Store factorial integer in RAX register for division in 'IntToStr'.
+    CALL IntToStr               ; Convert the integer in RAX to a string in 'buf' buffer.
 
     MOV RCX, STD_OUTPUT_HANDLE  ; Move STDOUT handle to RCX to be used by GetStdHandle call.
     CALL GetStdHandle           ; Receive console handle.
     MOV handle, RAX             ; Store console handle.
 
     MOV RCX, handle             ; 1st arg: handle to console screen buffer.
-    LEA RDX, txt                ; 2nd arg: pointer to buffer that contain text to write.
-    MOV R8, LENGTHOF txt        ; 3rd arg: number of characters to be written to console.
+    LEA RDX, buf                ; 2nd arg: pointer to buffer that contain text to write.
+    MOV R8, LENGTHOF buf        ; 3rd arg: number of characters to be written to console.
     LEA R9, num                 ; 4th arg: pointer to a variable to contain the number of characters actually written.
-    MOV QWORD PTR [RSP+32], 0   ; 5th arg; NULL.
+    MOV QWORD PTR [RSP+32], 0   ; 5th arg: lpReserved; NULL.
     CALL WriteConsoleA
 
     XOR RCX, RCX                ; Exit code 0;
-    CALL ExitProcess            ; Call the ExitProcess function to exit the program.
-main ENDP                       ; End of the main procedure.
-END                             ; End of the assembly program.
+    CALL ExitProcess
+main ENDP
+END
