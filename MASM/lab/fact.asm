5ca17fd0a8fc551d72c2e3c1a24370da05b3a5b1
Bryan C <7554866+bryancandi@users.noreply.github.com>
2026-03-10 19:10:45 -0700

    Update formatting for consistency

--- MASM/lab/fact.asm
+++ MASM/lab/fact.asm
@@ -5,109 +5,105 @@
 ; ml64.exe fact.asm /link /SUBSYSTEM:console /ENTRY:main
 ;--------------------------------------------------------------------
 
-INCLUDELIB kernel32.lib
-INCLUDELIB shell32.lib
+includelib kernel32.lib
+includelib shell32.lib
 
-ExitProcess PROTO
-GetStdHandle PROTO
-WriteConsoleA PROTO
-GetCommandLineW PROTO
-CommandLineToArgvW PROTO
+ExitProcess         proto
+GetStdHandle        proto
+WriteConsoleA       proto
+GetCommandLineW     proto
+CommandLineToArgvW  proto
 
-STD_OUTPUT_HANDLE EQU -11
+STD_OUTPUT_HANDLE   equ    -11
 
-.DATA
-    buf BYTE 100 DUP (?)        ; Empty 100 byte buffer to store ASCII digits of factorial result.
-    handle QWORD ?              ; 64-bit storage for Windows HANDLE (returned by GetStdHandle).
-    num DWORD ?                 ; 32-bit output variable for WriteConsoleA character count.
-    argc DWORD ?                ; 32‑bit storage for the argument count returned by CommandLineToArgvW.
-    argv QWORD ?                ; 64‑bit storage for the pointer to the argv array (LPWSTR*).
+        .data
+buf     byte    100 DUP (?)     ; Empty 100 byte buffer to store ASCII digits of factorial result.
+handle  qword   ?               ; 64-bit storage for Windows HANDLE (returned by GetStdHandle).
+num     dword   ?               ; 32-bit output variable for WriteConsoleA character count.
+argc    dword   ?               ; 32-bit storage for the argument count returned by CommandLineToArgvW.
+argv    qword   ?               ; 64-bit storage for the pointer to the argv array (LPWSTR*).
 
-.CODE
-IntToStr PROC
-    LEA RDI, buf                ; Load address of 'buf' buffer into RDI register.
-    ADD RDI, LENGTHOF buf - 1   ; Adjust pointer to end of 'buf' buffer.
-    MOV RBX, 10                 ; Move divisor into RBX.
+        .code
+IntToStr proc
+        lea     RDI, buf        ; Load address of 'buf' buffer into RDI register.
+        add     RDI, lengthof buf - 1 ; Adjust pointer to end of 'buf' buffer.
+        mov     RBX, 10         ; Move divisor into RBX.
 
-    div_loop:
-    XOR RDX, RDX                ; Clear for remainder.
-    DIV RBX                     ; Divide RAX by RBX (10).
-    ADD RDX, '0'                ; Add ASCII '0' value to remainder in RDX to convert from integer to ASCII value.
-    MOV [RDI], DL               ; Move one byte of RDX (DL) into RDI (RDI points to 'buf' buffer).
-    DEC RDI                     ; Decrement RDI; we are writing to the buffer in reverse order (starting at the end).
-    TEST RAX, RAX               ; Is RAX zero?
-    JNZ div_loop                ; No; restart loop.
-    LEA RAX, [RDI + 1]          ; Return value is stored in RAX. RAX + 1 because RDI was decremented before testing RAX for zero.
-    RET
-IntToStr ENDP
+divlp:  xor     RDX, RDX        ; Clear for remainder.
+        div     RBX             ; Divide RAX by RBX (10).
+        add     RDX, '0'        ; Add ASCII '0' value to remainder in RDX to convert from integer to ASCII value.
+        mov     [RDI], DL       ; Move one byte of RDX (DL) into RDI (RDI points to 'buf' buffer).
+        dec     RDI             ; Decrement RDI; we are writing to the buffer in reverse order (starting at the end).
+        test    RAX, RAX        ; Is RAX zero?
+        jnz     divlp           ; No; restart loop.
+        lea     RAX, [RDI + 1]  ; Return value is stored in RAX. RAX + 1 because RDI was decremented before testing RAX for zero.
+        ret
+IntToStr endp
 
-StrToInt PROC
-    XOR RAX, RAX
+StrToInt proc
+        xor     RAX, RAX
 
-    start:
-    MOVZX RDX, WORD PTR [RCX]   ; Move wchar with zero-extend into RDX.
-    TEST RDX, RDX               ; Is RDX zero?
-    JZ finish                   ; Yes; return.
-    SUB RDX, '0'                ; Subtract ASCII '0' value to convert from char to integer.
-    IMUL RAX, RAX, 10           ; Multiply the current accumulated value by 10 to shift its decimal place left before adding the next digit.
-    ADD RAX, RDX                ; Add the newly parsed digit (0–9) into the shifted accumulator to form the next partial value.
-    ADD RCX, 2                  ; Advance pointer to next wide character (wchar = 2 bytes).
-    JMP start
+start:  movzx   RDX, word ptr [RCX] ; Move wchar with zero-extend into RDX.
+        test    RDX, RDX        ; Is RDX zero?
+        jz      finish          ; Yes; return.
+        sub     RDX, '0'        ; Subtract ASCII '0' value to convert from char to integer.
+        imul    RAX, RAX, 10    ; Multiply the current accumulated value by 10 to shift its decimal place left before adding the next digit.
+        add     RAX, RDX        ; Add the newly parsed digit (0-9) into the shifted accumulator to form the next partial value.
+        add     RCX, 2          ; Advance pointer to next wide character (wchar = 2 bytes).
+        jmp     start
 
-    finish:
-    RET
-StrToInt ENDP
+finish:
+        ret
+StrToInt endp
 
-Factorial PROC
-    MOV RAX, 1                  ; Initialize factorial result to 1. RAX will hold the running factorial result.
-    CMP RCX, 1                  ; Check if RCX is less than or equal to 1 (fnum); return if true.
-    JLE finish
+Factorial proc
+        mov     RAX, 1          ; Initialize factorial result to 1. RAX will hold the running factorial result.
+        cmp     RCX, 1          ; Check if RCX is less than or equal to 1 (fnum); return if true.
+        jle     finish
 
-    start:
-    IMUL RAX, RCX               ; Multiply accumulator (RAX) by current RCX. RCX holds the value of 'fnum' initially.
-    DEC RCX                     ; Decrement loop counter RCX (fnum) by 1.
-    CMP RCX, 1                  ; Is RCX (fnum) still greater than 1? If so, continue multiplying.
-    JG start
+start:  imul    RAX, RCX        ; Multiply accumulator (RAX) by current RCX. RCX holds the value of 'fnum' initially.
+        dec     RCX             ; Decrement loop counter RCX (fnum) by 1.
+        cmp     RCX, 1          ; Is RCX (fnum) still greater than 1? If so, continue multiplying.
+        jg      start
 
-    finish:
-    RET
-Factorial ENDP
+finish: ret
+Factorial endp
 
-main PROC
-    SUB RSP, 40                 ; Create shadow space for 4 arguments (32 shadow + 8 alignment).
+main    proc
+        sub     RSP, 40         ; Create shadow space for 4 arguments (32 shadow + 8 alignment).
 
-    XOR RAX, RAX                            ; Clear registers.
-    XOR RCX, RCX
-    XOR RDX, RDX
-    XOR R8, R8
-    XOR R9, R9
+        xor     RAX, RAX        ; Clear registers.
+        xor     RCX, RCX
+        xor     RDX, RDX
+        xor     R8, R8
+        xor     R9, R9
 
-    CALL GetCommandLineW
-    MOV RCX, RAX                ; LPWSTR cmdline.
-    LEA RDX, argc               ; int argc.
+        call    GetCommandLineW
+        mov     RCX, RAX        ; LPWSTR cmdline.
+        lea     RDX, argc       ; int argc.
 
-    CALL CommandLineToArgvW
-    MOV argv, RAX               ; LPWSTR* argv.
-    MOV RCX, [argv]             ; Point to argv array in RCX.
-    MOV RCX, [RCX+8]            ; Point to argv[1] (skip program name).
+        call    CommandLineToArgvW
+        mov     argv, RAX       ; LPWSTR* argv.
+        mov     RCX, [argv]     ; Point to argv array in RCX.
+        mov     RCX, [RCX+8]    ; Point to argv[1] (skip program name).
 
-    CALL StrToInt               ; Convert the string in argv[1] to an integer to use in factorial calculation.
-    MOV RCX, RAX                ; Move number to calculate the factorial of into RCX register.
-    CALL Factorial              ; Store factorial integer in RAX register for division in 'IntToStr'.
-    CALL IntToStr               ; Convert the integer in RAX to a string in 'buf' buffer.
+        call    StrToInt        ; Convert the string in argv[1] to an integer to use in factorial calculation.
+        mov     RCX, RAX        ; Move number to calculate the factorial of into RCX register.
+        call    Factorial       ; Store factorial integer in RAX register for division in IntToStr.
+        call    IntToStr        ; Convert the integer in RAX to a string in 'buf' buffer.
 
-    MOV RCX, STD_OUTPUT_HANDLE  ; Move STDOUT handle to RCX to be used by GetStdHandle call.
-    CALL GetStdHandle           ; Receive console handle.
-    MOV handle, RAX             ; Store console handle.
+        mov     RCX, STD_OUTPUT_HANDLE ; Move STDOUT handle to RCX to be used by GetStdHandle call.
+        call    GetStdHandle    ; Receive console handle.
+        mov     handle, RAX     ; Store console handle.
 
-    MOV RCX, handle             ; 1st arg: handle to console screen buffer.
-    LEA RDX, buf                ; 2nd arg: pointer to buffer that contain text to write.
-    MOV R8, LENGTHOF buf        ; 3rd arg: number of characters to be written to console.
-    LEA R9, num                 ; 4th arg: pointer to a variable to contain the number of characters actually written.
-    MOV QWORD PTR [RSP+32], 0   ; 5th arg: lpReserved; NULL.
-    CALL WriteConsoleA
+        mov     RCX, handle     ; 1st arg: handle to console screen buffer.
+        lea     RDX, buf        ; 2nd arg: pointer to buffer that contain text to write.
+        mov     R8, lengthof buf ; 3rd arg: number of characters to be written to console.
+        lea     R9, num         ; 4th arg: pointer to a variable to contain the number of characters actually written.
+        mov     qword ptr [RSP+32], 0 ; 5th arg: lpReserved; NULL.
+        call    WriteConsoleA
 
-    XOR RCX, RCX                ; Exit code 0;
-    CALL ExitProcess
-main ENDP
-END
+        xor     RCX, RCX        ; Exit code 0.
+        call    ExitProcess
+main    endp
+        end
