2c8e2c838909b3a22622b776d6c0c20a47157355
Bryan C <7554866+bryancandi@users.noreply.github.com>
2026-03-21 08:24:07 -0700

    Implement macros for console input and output (strings and buffers)

--- MASM/lab/echo_uni.asm
+++ MASM/lab/echo_uni.asm
@@ -1,10 +1,11 @@
 ;-------------------------------------------------------------------------
-; x64 UNICODE ECHO program for Windows console.
+; x64 ECHO (v2) program for Windows console.
+; Uses macros and ReadConsoleW/WriteConsoleW for UTF-16 I/O.
 ; Repeatedly reads a line of input and echoes it back to the console.
 ; Terminates when an empty line (CRLF only) is entered.
 ;
 ; Assemble with MASM and link:
-; ml64.exe echo.asm /link /SUBSYSTEM:console /ENTRY:main
+; ml64.exe echo_uni.asm /link /SUBSYSTEM:console /ENTRY:main
 ;-------------------------------------------------------------------------
 
 includelib kernel32.lib                     ; Import Kernel32 library (Windows API).
@@ -16,7 +17,31 @@ WriteConsoleW   proto                       ; Write a buffer of characters to th
 
 STD_INPUT_HANDLE  equ    -10                ; Device code for keyboard input.
 STD_OUTPUT_HANDLE equ    -11                ; Device code for console output.
-MaxBuf            equ    100                ; Maximum input buffer size.
+MaxBuf            equ    200                ; Maximum input buffer size.
+
+readIn  macro   buf                         ; Single argument macro to read user input into a buffer (ReadConsoleW).
+        mov     RCX, stdin                  ; Arg 1: input device handle.
+        lea     RDX, buf                    ; Arg 2: pointer to a buffer that receives the data read from the console input buffer.
+        mov     R8, MaxBuf                  ; Arg 3: number of UTF‑16 characters to read.
+        lea     R9, nbrd                    ; Arg 4: pointer to variable that receives number of characters read.
+        call    ReadConsoleW                ; Function call to read user input.
+        endm
+
+strOut  macro   msg                         ; Single argment macro to call write a string to console (WhiteConsoleW).
+        mov     RCX, stdout                 ; Arg 1: output device handle.
+        lea     RDX, msg                    ; Arg 2: pointer to character array.
+        mov     R8, lengthof msg            ; Arg 3: number of UTF‑16 characters to write.
+        lea     R9, nbwr                    ; Arg 4: pointer to variable that receives number of bytes written.
+        call    WriteConsoleW               ; Function call to write to console.
+        endm
+
+bufOut  macro   buf                         ; Single argment macro to call write contents of a buffer to console (WhiteConsoleW).
+        mov     RCX, stdout                 ; Arg 1: output device handle.
+        lea     RDX, buf                    ; Arg 2: pointer to character array.
+        mov     R8d, [nbrd]                 ; Arg 3: number of UTF‑16 characters to write.
+        lea     R9, nbwr                    ; Arg 4: pointer to variable that receives number of bytes written.
+        call    WriteConsoleW               ; Function call to write to console.
+        endm
 
         .data
 pmsg    word    "E","n","t","e","r"," ","t","e","x","t",":"," " ; User prompt message.
@@ -41,36 +66,24 @@ main    proc
         mov     stdout, RAX                 ; Store the handle for console output.
 
 ;       Print prompt to console.
-next:   mov     RCX, stdout                 ; Arg 1: output device handle.
-        lea     RDX, pmsg                   ; Arg 2: pointer to byte array.
-        mov     R8, lengthof pmsg           ; Arg 3: number of UTF‑16 characters to write.
-        lea     R9, nbwr                    ; Arg 4: pointer to variable that receives number of bytes written.
-        call    WriteConsoleW               ; Function call to write text to console.
+next:   strOut  pmsg
 
 ;       Read user input.
-        mov     RCX, stdin                  ; Arg 1: input device handle.
-        lea     RDX, inbuf                  ; Arg 2: pointer to a buffer that receives the data read from the console input buffer.
-        mov     R8, MaxBuf                  ; Arg 3: number of UTF‑16 characters to read.
-        lea     R9, nbrd                    ; Arg 4: pointer to variable that receives number of bytes read.
-        call    ReadConsoleW                ; Function call to read user keyboard input.
+        readIn  inbuf
 
 ;       Exit if empty line, otherwise continue.
-        mov     R8d, [nbrd]                 ; Copy length in bytes of input message (32-bit DWORD).
-        cmp     R8d, 2                      ; If only CRLF (2 chars) was entered, treat as empty line.
-        je      exit                        ; Jump to exit if equal.
+        mov     R8d, [nbrd]                 ; Number of UTF-16 characters read (DWORD; use R8d to load a 32-bit value).
+        cmp     R8d, 2                      ; CRLF (2 chars) only?
+        je      exit
 
-;       Print to console.
-        mov     RCX, stdout                 ; Arg 1: output device handle.
-        lea     RDX, inbuf                  ; Arg 2: pointer to byte array.
-        mov     R8d, [nbrd]                 ; Arg 3: number of UTF‑16 characters to write.
-        lea     R9, nbwr                    ; Arg 4: pointer to variable that receives number of bytes written.
-        call    WriteConsoleW               ; Function call to write text to console.
+;       Print buffer to console.
+        bufOut  inbuf
 
 ;       Get next input from user.
-        jmp     next                        ; Repeat loop.
+        jmp     next
 
 ;       Program exit.
-exit:   xor     RCX, RCX                    ; Set exit status code to zero.
-        call    ExitProcess                 ; Call the ExitProcess function to exit the program.
+exit:   xor     RCX, RCX
+        call    ExitProcess
 main    endp
         end
