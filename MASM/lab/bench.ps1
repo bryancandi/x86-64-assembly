b5c5be881ca55e6b42a4571799fc67d479d3d0ab
Bryan C <7554866+bryancandi@users.noreply.github.com>
2026-04-21 11:41:24 -0700

    Initial commit

---
+++ MASM/lab/bench.ps1
@@ -0,0 +1,13 @@
+$sw = [System.Diagnostics.Stopwatch]::StartNew()
+
+$runs = 100
+$seqs = 370
+
+for ($i = 1; $i -le $runs; $i++) {
+    $seqs | .\fibonacci256.exe > $null
+}
+
+$sw.Stop()
+
+Write-Host "Runs: $runs"
+Write-Host "Total ms:" $sw.ElapsedMilliseconds
