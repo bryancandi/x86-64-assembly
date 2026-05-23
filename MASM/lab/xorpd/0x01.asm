21a2eb034242499afed28c0e15d7e54daf6e8634
Bryan C <7554866+bryancandi@users.noreply.github.com>
2026-05-22 20:26:01 -0700

    Initial commit

---
+++ MASM/lab/xorpd/0x01.asm
@@ -0,0 +1,27 @@
+;======================================
+; Fibonacci Sequence
+; 0x01 - xorpd
+;======================================
+
+INCLUDELIB kernel32.lib
+
+ExitProcess PROTO uExitCode:DWORD
+
+OPTION DOTNAME
+
+        .CODE
+start   PROC
+        sub     rsp, 40
+
+        mov     rax, 0                  ; Initialize value 1 for Fibonacci sequence
+        mov     rdx, 1                  ; Initialize value 2 for Fibonacci sequence
+        mov     rcx, 20                 ; Initialize counter
+
+.loop:
+        xadd    rax, rdx                ; RDX = RAX; RAX += RDX
+        loop    .loop                   ; Loop until RCX = 0
+
+        xor     ecx, ecx
+        call    ExitProcess
+start   ENDP
+        END
