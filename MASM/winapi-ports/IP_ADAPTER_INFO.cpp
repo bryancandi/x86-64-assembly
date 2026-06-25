86f4d73256abf7f3a5a94fbf505da4d3ca81827c
Bryan C <7554866+bryancandi@users.noreply.github.com>
2026-06-24 23:09:47 -0700

    Add IP_ADDR_STRING structure definition

--- MASM/winapi-ports/IP_ADAPTER_INFO.cpp
+++ MASM/winapi-ports/IP_ADAPTER_INFO.cpp
@@ -3,6 +3,13 @@
 #include <winsock2.h>
 #include <iphlpapi.h>
 
+typedef struct _IP_ADDR_STRING {
+  struct _IP_ADDR_STRING *Next;
+  IP_ADDRESS_STRING      IpAddress;
+  IP_MASK_STRING         IpMask;
+  DWORD                  Context;
+} IP_ADDR_STRING, *PIP_ADDR_STRING;
+
 typedef struct _IP_ADAPTER_INFO {
   struct _IP_ADAPTER_INFO *Next;
   DWORD                   ComboIndex;
