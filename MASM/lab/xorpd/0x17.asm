fab53496dc1cad19b2c67a2b00498f80c023a040
Bryan C <7554866+bryancandi@users.noreply.github.com>
2026-07-13 07:37:02 -0700

    Initial commit

---
+++ MASM/lab/xorpd/0x17.asm
@@ -0,0 +1,40 @@
+;======================================================================
+; Source : xchg rax,rax (xorpd)
+; Index  : 0x17
+;
+; If RAX is negative, this is equivalent to `neg rax`
+; If RAX is positive, it remains unchanged
+;
+; Example showing only the lower 8-bits of RAX and RDX
+; Sign-extend RAX into RDX:RAX (cqo)
+;   RAX:    11111010    -6
+;   RDX:    11111111    -1
+;
+; Invert bits in RAX (xor)
+;   RAX:    00000101    5
+;
+; Subtract: 5 - (-1) = 6
+;   RAX:    00000101
+;   RDX:    11111111
+; = RAX:    00000110
+; 
+;======================================================================
+
+INCLUDELIB kernel32.lib
+
+ExitProcess PROTO uExitCode:DWORD
+
+        .CODE
+start   PROC
+        sub     rsp, 40
+
+        mov     rax, 0fffffffffffffffah                ; Initialize RAX
+
+        cqo
+        xor     rax, rdx
+        sub     rax, rdx
+
+        xor     ecx, ecx
+        call    ExitProcess
+start   ENDP
+        END
