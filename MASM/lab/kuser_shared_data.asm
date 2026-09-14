6533421af20ba6d51cd4206c35683ccf53aeaff5
Bryan C <7554866+bryancandi@users.noreply.github.com>
2026-09-14 09:34:51 -0700

    Adjust hexadecimal notation for memory address

--- MASM/lab/kuser_shared_data.asm
+++ MASM/lab/kuser_shared_data.asm
@@ -1,4 +1,4 @@
-; 07FFE0000h
+; 7FFE0000h
 ; Accessing values in KUSER_SHARED_DATA structure by offsets
 
 INCLUDELIB kernel32.lib
@@ -11,7 +11,7 @@ ExitProcess PROTO
 start   PROC
         sub rsp, 28h
 
-        mov rbx, 07FFE0000h
+        mov rbx, 7FFE0000h
         mov eax, [rbx + 026Ch]      ; NtMajorVersion
         mov ecx, [rbx + 0270h]      ; NtMinorVersion
         mov edx, [rbx + 0260h]      ; NtBuildNumber
