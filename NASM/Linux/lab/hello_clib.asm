68ecae39a457c0c50a73134d0c76b63c82054823
Bryan C <7554866+bryancandi@users.noreply.github.com>
2026-09-15 19:51:25 -0700

    Initial commit

---
+++ NASM/Linux/lab/hello_clib.asm
@@ -0,0 +1,26 @@
+; hello, world!
+; example with printf
+
+; Set 64-bit mode and RIP-relative addressing mode
+bits 64
+default rel
+
+section .data
+
+msg     db  "hello, world!", 10, 0
+fmt1    db  "%s", 0
+
+section .text
+
+extern printf
+
+global main
+main:
+        sub     rsp, 8
+
+        lea     rdi, fmt1
+        lea     rsi, msg
+        call    printf wrt ..plt
+
+        add     rsp, 8
+        ret
